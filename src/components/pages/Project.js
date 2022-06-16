3d552d9f41d27e4603efde6e0483934d865ad113
flaviojnr100 <flaviojunior1009@gmail.com>
2022-06-16 17:37:52 -0300

    Remoção de serviços

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -93,8 +93,34 @@ function Project(){
     }).catch((er)=>console.log(er))
     }
 
-    function removeService(){
+    function removeService(id, cost){
+        setMessage('')
+        const servicesUpdated = project.services.filter(
+            (service)=> service.id !== id
+        )
 
+        const projectUpdated = project
+
+        projectUpdated.services = servicesUpdated
+        projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost)
+
+        fetch(`http://localhost:5000/projects/${projectUpdated.id}`,
+        {
+            method:'PUT',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(projectUpdated)
+        }
+        )
+        .then((res)=>res.json())
+        .then((data)=>{
+            setProject(projectUpdated)
+            setServices(servicesUpdated)
+            setMessage('Serviço removido com sucesso!')
+            setType('sucess')
+        })
+        .catch((err)=>console.log(err))
     }
 
     function toggleProjectForm(){
